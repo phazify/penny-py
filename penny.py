b73f76d3a570a5faabcbfb159602aedc1b00bfaf
phazify <101293535+phazify@users.noreply.github.com>
2022-05-10 15:41:59 +0200

    Create penny.py

---
+++ penny.py
@@ -0,0 +1,128 @@
+import getpass
+import colorama
+from colorama import Fore
+import discord
+from discord.ext import commands
+import os
+import getpass
+client = commands.Bot(command_prefix=commands.when_mentioned_or("$"))
+
+os.system(f'cls & mode 85,20 & title PHAZIFY AUTOPRESSER')
+user = input("Name: ")
+print(f'''{Fore.GREEN}
+ _    __   __    __   ______   ________  ______  ________  __      __ 
+|       \ |  \  |  \ /      \ |        \|      \|        \|  \    /  |
+| $$$$$$$\| $$  | $$|  $$$$$$\ \$$$$$$$$ \$$$$$$| $$$$$$$$ \$$\  /  $$
+| $$__/ $$| $$__| $$| $$__| $$    /  $$   | $$  | $$__      \$$\/  $$ 
+| $$    $$| $$    $$| $$    $$   /  $$    | $$  | $$  \      \$$  $$  
+| $$$$$$$ | $$$$$$$$| $$$$$$$$  /  $$     | $$  | $$$$$       \$$$$   
+| $$      | $$  | $$| $$  | $$ /  $$___  _| $$_ | $$          | $$    
+| $$      | $$  | $$| $$  | $$|  $$    \|   $$ \| $$          | $$    
+ \$$       \$$   \$$ \$$   \$$ \$$$$$$$$ \$$$$$$ \$$           \$$    
+             ___________LOGGED IN AS: {user}___________                                             
+                       P1 X PHAZIFY MADE THIS
+                                                                                    
+        ''' + Fore.GREEN)
+
+token = input("Enter token: ")
+sex = input("Keyword: ")
+print("READY TO RAPE BLACKS")
+###### CODEZZZZZ#######
+@client.event
+async def on_message(message):
+    channel = message.channel
+    print("BITCH GOT RAPED:",message.content)
+    if message.content.endswith(sex):
+
+        await message.channel.send('KKK RUNS YOU ')
+        await message.channel.send('YOU LOOK LIKE A DARK SKIN LO KUNG FROM WAKANDA')
+        await message.channel.send('🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪🔪')
+        await message.channel.send('LAME GOOFY FUCK EGO ME AND ILL SLEEP YOU')
+        await message.channel.send('SONNY BOY GET UR SLEEPING MASK OUT')
+        await message.channel.send('LOOOL YOU CANT LAST WITH A ABSOLUTE DEMON')
+        await message.channel.send('WATCH ME MAKE YOU SLEEP')
+        await message.channel.send('UR MOMS LOCKED UP IN A SOLITARY CONFINEMENT')
+        await message.channel.send('FUCKING GHETTO BITCH')
+        await message.channel.send('GET YOUR ⚰️ READY UR GONNA GO 😴')
+        await message.channel.send('LOOOLOOOLLOOOLL RATIO + BOZO + L')
+        await message.channel.send('SURRENDER YOU CANT OUTLAST ME LMAO')
+        await message.channel.send('REPENT YOURSELF FAGGOT')
+        await message.channel.send('LOOOLL ILL CREMATE YOU WEAK ALT')
+        await message.channel.send('UR MOMS TRANNY DICK LONGER THAN UR FRIENDLIST')
+        await message.channel.send('UNKNOWN FUCK CHATTING SHIT?')
+        await message.channel.send('HOP ON VC AND TRY ME SON LOOL')
+        await message.channel.send('I HEADBUNCHED UR MOM TO SLEEP')
+        await message.channel.send('UR MOM TRIED CURING HER STD')
+        await message.channel.send('CLENCHED MY SON ASLEEP')
+        await message.channel.send('NAZI RUNS YOU')
+        await message.channel.send('UR DAD USES JELLYFISH JAM AND EXPIRED EGGS AS LUBE')
+        await message.channel.send('WEIRD BUM')
+        await message.channel.send('STUPID ASS NIGGA WILL SLUMP SOON')
+        await message.channel.send('KEEP CHATMOVING WITH UR FATHER')
+        await message.channel.send('LOOOLOOOL')
+        await message.channel.send('LOLOLO')
+        await message.channel.send('SHITTY ASS PACKER')
+        await message.channel.send('FUCK U TRYNA MESS WITH A LIEUTENANT FOR?')
+        await message.channel.send('KEEP WAFFLING SON')
+        await message.channel.send('DONT BLINK OR YAWN FOCUS')
+        await message.channel.send('ILL MOCK YOU DOWN LIKE IM CAPTIAN JACK SPARROW')
+        await message.channel.send('BACK INSIDE THE CAGE YOU GORILLA')
+        await message.channel.send('DOG ON THE LEASH')
+        await message.channel.send('SON NEEDS TO BE RERAISED?')
+        await message.channel.send('WATCH THAT ATTITUDE SON')
+        await message.channel.send('FUCK WRONG WITH UR NASTY ASS')
+        await message.channel.send('SAY CHEESE 📷📷📷📷📷📷📷📷📷📷📷📷')
+        await message.channel.send('PUTTING UR MOMS SEXTAPE ON TELEGRAM')
+        await message.channel.send('FUCKING TROLL')
+        await message.channel.send('MY DICK LONGER THAN UR MOMS PORN CAREER')
+        await message.channel.send('MADE UR MOM MEET JESUS LOLOOL')
+        await message.channel.send('CRACKED BITCH LOOLOL')
+        await message.channel.send('SONS TRYNA COMPETE AGAINST A GENERAL??')
+        await message.channel.send('LOOOL TALIBAN RUNS YOU WEAK FUCK')
+        await message.channel.send('EMO ASS BITCH')
+        await message.channel.send('BEG YOUR ALMIGHTY FATHER FOR FORGIVENESS')
+        await message.channel.send('I CANT LAST 🤓🤓🤓')
+        await message.channel.send('FOCUS U STILL THERE???? TIRED ALREADY??')
+        await message.channel.send('DIRTY ASS BOY DROP UR BALLS THEN PACK ME')
+        await message.channel.send('PUT PHAZIFYS JR ON UR NAME OR IM ENDING YOU')
+        await message.channel.send('OR MOMS A FUCKING MIDDLE EAST SLAVE')
+        await message.channel.send('SON BE BENDING DOWN TO PICK UP COTTONS')
+        await message.channel.send('WE PACKING TILL THE SUNS RUNS OUT OF FUEL')
+        await message.channel.send('LOLOLOL')
+        await message.channel.send('OLOLOL')
+        await message.channel.send('LOLOLOL')
+        await message.channel.send('HEAD UP')
+        await message.channel.send('SONS HAVING 0 GRAVITY')
+        await message.channel.send('UR MOM IS A FUCKING SEXWORKER')
+        await message.channel.send('U SUCK')
+        await message.channel.send('RETARDED ASS BOY')
+        await message.channel.send('UR MOMS WEARS A FUCKING WIG')
+        await message.channel.send('THAT BITCH HAS PUBIC HAIR ON HER TITS')
+        await message.channel.send('GOOFY ASS BOY NEEDS A BREAK?')
+        await message.channel.send('CHEETO ASS NIGGA')
+        await message.channel.send('SON CONTINUE EATING RAW MEAT WITH GRARLIC SAUCE')
+        await message.channel.send('MY MAN HAS A 3D SPONGEBOB UNDERWEAR')
+        await message.channel.send('UR MOM GOT CURSED BY 2 WITCHES LOOOL')
+        await message.channel.send('SONS CURSE NUMBER IS 10')
+        await message.channel.send('WE ARE RUNNING THE MARATHON FOCUS')
+        await message.channel.send('STAY HYDRATED')
+        await message.channel.send('IF YOU SLEEP YOUR MY GOON FOR LIFE')
+        await message.channel.send('LOOL VIRTUAL BITCH')
+        await message.channel.send('YOU CANNOT PRESSURE A PRESSURE DEMON')
+        await message.channel.send('LOOLOOOLOLOL')
+        await message.channel.send('UR LIFE DESTINY IS LOSING TO ME')
+        await message.channel.send('YOUR MUM GOT SOLD IN A BLACK MARKET AUCTION')
+        await message.channel.send('FUCKING PYSCHO BITCH')
+        await message.channel.send('UR MOMS UNSTOPPABLE CUPSIZE A TITS')
+        await message.channel.send('OOOLOOL ILL MAKE A BLOODBATH WITH UR FAMILY TREE INSIDE')
+        await message.channel.send('UR DAD IS A FUCKING FARMER')
+        await message.channel.send('U WEAR CLOGS TO SCHOOL')
+        await message.channel.send('UNWANTED BITCH')
+        await message.channel.send('WASTE OF SPERM')
+        await message.channel.send('MY NIGGAH REALLY WAS TRYNA FUCK A PIG IRL')
+        await message.chandel.send('SON WANTS TO GET TAMED WITH A WHIP')
+        await message.channel.send('911')
+        await message.channel.send('SON HAS A DEATHWISE')
+        await message.channel.send('LOLOL FUCKING BUM')
+        
+client.run(token, bot=False)
